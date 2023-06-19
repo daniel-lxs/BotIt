21b1df663db9120d7db5b9b4e39f8d6cc190cf75
Daniel Riccio <ricciodaniel98@gmail.com>
2023-06-19 16:51:04 -0500

    create barrel files

---
+++ src/lemmy/index.ts
@@ -0,0 +1,6 @@
+export * from './service/parseRawPostsToUrls';
+export * from './api/createPost';
+export * from './api/getCommunityId';
+export * from './api/getCommunityPosts';
+export * from './api/getJwt';
+export * from './model/Post';
